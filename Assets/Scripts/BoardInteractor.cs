131ebf8235eba14d1f89d68459673204bf1067b3
Dmytro Maretskyi <35851437+Marik-D@users.noreply.github.com>
2021-03-27 18:19:20 +0200

    Extract BoardState

--- Assets/Scripts/BoardInteractor.cs
+++ Assets/Scripts/BoardInteractor.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using DefaultNamespace;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -8,11 +9,17 @@ public class BoardInteractor : MonoBehaviour
 {
     public GameObject redDotPrefab;
     public GameObject blueDotPrefab;
-    
+
+    private BoardState _state;
+    private SpriteRenderer _spriteRenderer;
+
     // Start is called before the first frame update
     void Start()
     {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
         
+        var gridSize = _spriteRenderer.size / 0.64f;
+        _state = new BoardState((int)gridSize.y, (int)gridSize.x);
     }
 
     // Update is called once per frame
@@ -23,24 +30,40 @@ public class BoardInteractor : MonoBehaviour
             var cursorWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             cursorWorldPos.z = 0;
 
-            
             var cursorLocalPos = transform.InverseTransformPoint(cursorWorldPos);
-            var spriteSize = GetComponent<SpriteRenderer>().size;
-            var gridSize = spriteSize / 0.64f;
 
-            var cellIdx = ((Vector2)cursorLocalPos + spriteSize / 2) / 0.64f;
+            var cellIdx = ((Vector2)cursorLocalPos + _spriteRenderer.size / 2) / 0.64f;
 
             int cellCol = Mathf.RoundToInt(cellIdx.x) - 1;
             int cellRow = Mathf.RoundToInt(cellIdx.y) - 1;
 
-            if (cellCol >= 0 && cellCol < gridSize.x - 2 && cellRow >= 0 && cellRow < gridSize.y - 2)
+            if (_state.CanPlace(cellRow, cellCol))
             {
                 Debug.Log($"Placing at row={cellRow} col={cellCol}");
+                _state.Place(cellRow, cellCol);
                 
-                var dotLocation = new Vector2(cellCol + 1, cellRow + 1) * 0.64f - spriteSize / 2;
-                
-                Instantiate(redDotPrefab, transform.TransformPoint(dotLocation), Quaternion.identity, transform);
+                SyncBoardState();
             } 
+        }
+    }
+
+    void SyncBoardState()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Destroy(transform.GetChild(i).gameObject);                
+        }
+        for (int i = 0; i < _state.rows; i++)
+        {
+            for (int j = 0; j < _state.cols; j++)
+            {
+                var cell = _state.Get(i, j);
+                if (cell.IsPlaced)
+                {
+                    var dotLocation = new Vector2(j + 1, i + 1) * 0.64f - _spriteRenderer.size / 2;
+                    Instantiate(cell.Player == Player.Red ? redDotPrefab : blueDotPrefab, transform.TransformPoint(dotLocation), Quaternion.identity, transform);
+                }
+            }
         }
     }
 }
