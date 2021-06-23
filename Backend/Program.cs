b8fe412d4f74f8565ad548b2c16c5c36d9f18e07
Dmytro Maretskyi <35851437+Marik-D@users.noreply.github.com>
2021-06-23 18:15:27 +0300

    Add backend project with a basic socket example

---
+++ Backend/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net;
+
+namespace Backend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int port = 8080;
+            var server = new SocketServer(IPAddress.Any, port);
+
+            // Start the server
+            Console.Write("Server starting...");
+            server.Start();
+            Console.WriteLine("Done!");
+
+            Console.WriteLine("Press Enter to stop the server or '!' to restart the server...");
+
+            // Perform text input
+            for (;;)
+            {
+                string line = Console.ReadLine();
+                if (string.IsNullOrEmpty(line))
+                    break;
+
+                // Restart the server
+                if (line == "!")
+                {
+                    Console.Write("Server restarting...");
+                    server.Restart();
+                    Console.WriteLine("Done!");
+                }
+
+                // Multicast admin message to all sessions
+                line = "(admin) " + line;
+                server.MulticastText(line);
+            }
+
+            // Stop the server
+            Console.Write("Server stopping...");
+            server.Stop();
+            Console.WriteLine("Done!");
+        }
+    }
+}
---
+++ Backend/SocketServer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using NetCoreServer;
+
+namespace Backend
+{
+    public class SocketServer : WsServer
+    {
+        public SocketServer(IPAddress address, int port) : base(address, port) { }
+
+        protected override TcpSession CreateSession() { return new SocketSession(this); }
+
+        protected override void OnError(SocketError error)
+        {
+            Console.WriteLine($"Chat WebSocket server caught an error with code {error}");
+        }
+    }
+}
